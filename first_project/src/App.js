76181be19900cd21f83535b1fb5d4fff9b4cce8d
Selvarani CR <Selvarani.cr@datagrokr.com>
2024-04-02 14:54:31 +0530

    updated

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import UserGreeting from './components/UserGreeting'
-
+import NameList from './components/NameList';
+import CakeContainer from './components/CakeContainer';
 function App() {
   return (
     <div className="App">
-      <UserGreeting />
+      <CakeContainer />
+      {/* <UserGreeting /> */}
+      {/* <NameList /> */}
     </div>
   );
 }
